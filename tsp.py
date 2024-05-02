e9804ec8b06accbcaf5b83cf91591bd4618a0362
Sushrutha Reddy Mogili <104207575+sushrutha29@users.noreply.github.com>
2024-05-02 17:41:33 -0500

    Update tsp.py

--- tsp.py
+++ tsp.py
@@ -1,4 +1,6 @@
 import sys
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def traveling_salesman(graph, startnode):
     n = len(graph)
@@ -18,18 +20,35 @@ def traveling_salesman(graph, startnode):
 
         memory[(currentnode, visitednode)] = min_cost
         return min_cost
+    
     return traveling_salesman_helper(startnode, 1 << startnode)
+
 graph = [
-    [1, 12, 39, 0],
-    [0, 18, 55, 32],
-    [21, 34, 20, 0],
-    [0, 15, 20, 0]
+    [1, 14, 39, 0],
+    [0, 18, 55, 44],
+    [21, 23, 20, 0],
+    [0, 13, 20, 0]
 ]
 start_city = 0 
 min_cost = traveling_salesman(graph, start_city)
-print("Minimum cost of TSP tour:", min_cost)
+print("Minimum cost:", min_cost)
+
+# Create a graph from the input graph
+G = nx.Graph()
+for i in range(len(graph)):
+    for j in range(len(graph)):
+        if graph[i][j] != 0:
+            G.add_edge(i, j, weight=graph[i][j])
+
+# Plot the graph
+pos = nx.spring_layout(G)
+plt.figure(figsize=(4, 5))
+nx.draw(G, pos, with_labels=True, node_size=2000, node_color='white', font_size=20, font_weight='bold')
+plt.title("TSP Tour")
+plt.show()
+
 
 '''
 output:
-Minimum cost of TSP tour: 54
+Minimum cost : 43
 '''
